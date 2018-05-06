746afcf412745bb31c6c252a959c39ccbe22a431
Gooddjamp <Gooddjamp@mail.ru>
2018-05-06 17:15:10 +0300

    first run aplication

---
+++ ble_app_template/obj/Debug/nRF5_SDK_12.3.0_d7731ad/components/libraries/bsp/bsp_nfc.d
@@ -0,0 +1,30 @@
+obj\debug\nrf5_sdk_12.3.0_d7731ad\components\libraries\bsp\bsp_nfc.o: \
+ ..\nRF5_SDK_12.3.0_d7731ad\components\libraries\bsp\bsp_nfc.c \
+ ..\nRF5_SDK_12.3.0_d7731ad\components\libraries\bsp\bsp_nfc.h \
+ ..\nRF5_SDK_12.3.0_d7731ad\components\libraries\util/sdk_errors.h \
+ ..\nRF5_SDK_12.3.0_d7731ad\components\softdevice\s130\headers/nrf_error.h \
+ ..\pca10028\s130\config/sdk_config.h \
+ ..\nRF5_SDK_12.3.0_d7731ad\components\libraries\bsp\bsp.h \
+ ..\nRF5_SDK_12.3.0_d7731ad\components\boards/boards.h \
+ ..\nRF5_SDK_12.3.0_d7731ad\components\drivers_nrf\hal/nrf_gpio.h \
+ ..\nRF5_SDK_12.3.0_d7731ad\components\device/nrf.h \
+ ..\nRF5_SDK_12.3.0_d7731ad\components\device/nrf51.h \
+ ..\nRF5_SDK_12.3.0_d7731ad\components\toolchain\cmsis\include/core_cm0.h \
+ ..\nRF5_SDK_12.3.0_d7731ad\components\toolchain\cmsis\include/core_cmInstr.h \
+ ..\nRF5_SDK_12.3.0_d7731ad\components\toolchain\cmsis\include/cmsis_gcc.h \
+ ..\nRF5_SDK_12.3.0_d7731ad\components\toolchain\cmsis\include/core_cmFunc.h \
+ ..\nRF5_SDK_12.3.0_d7731ad\components\toolchain/system_nrf51.h \
+ ..\nRF5_SDK_12.3.0_d7731ad\components\device/nrf51_bitfields.h \
+ ..\nRF5_SDK_12.3.0_d7731ad\components\device/nrf51_deprecated.h \
+ ..\nRF5_SDK_12.3.0_d7731ad\components\device/compiler_abstraction.h \
+ ..\nRF5_SDK_12.3.0_d7731ad\components\drivers_nrf\hal/nrf_peripherals.h \
+ ..\nRF5_SDK_12.3.0_d7731ad\components\device/nrf51822_peripherals.h \
+ ..\nRF5_SDK_12.3.0_d7731ad\components\libraries\util/nrf_assert.h \
+ ..\nRF5_SDK_12.3.0_d7731ad\components\libraries\util/app_error.h \
+ ..\nRF5_SDK_12.3.0_d7731ad\components\libraries\util/sdk_errors.h \
+ ..\nRF5_SDK_12.3.0_d7731ad\components\libraries\util/nordic_common.h \
+ ..\nRF5_SDK_12.3.0_d7731ad\components\libraries\util/app_error_weak.h \
+ ..\nRF5_SDK_12.3.0_d7731ad\components\boards/pca10028.h \
+ ..\nRF5_SDK_12.3.0_d7731ad\components\libraries\button/app_button.h \
+ ..\nRF5_SDK_12.3.0_d7731ad\components\libraries\util/app_error.h \
+ ..\nRF5_SDK_12.3.0_d7731ad\components\drivers_nrf\hal/nrf_peripherals.h
--- main.c
+++ main.c
@@ -1,30 +1,30 @@
 /**
  * Copyright (c) 2014 - 2017, Nordic Semiconductor ASA
- * 
+ *
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice, this
  *    list of conditions and the following disclaimer.
- * 
+ *
  * 2. Redistributions in binary form, except as embedded into a Nordic
  *    Semiconductor ASA integrated circuit in a product or a software update for
  *    such product, must reproduce the above copyright notice, this list of
  *    conditions and the following disclaimer in the documentation and/or other
  *    materials provided with the distribution.
- * 
+ *
  * 3. Neither the name of Nordic Semiconductor ASA nor the names of its
  *    contributors may be used to endorse or promote products derived from this
  *    software without specific prior written permission.
- * 
+ *
  * 4. This software, with or without modification, must only be used with a
  *    Nordic Semiconductor ASA integrated circuit.
- * 
+ *
  * 5. Any software provided in binary form under this license must not be reverse
  *    engineered, decompiled, modified and/or disassembled.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY NORDIC SEMICONDUCTOR ASA "AS IS" AND ANY EXPRESS
  * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
  * OF MERCHANTABILITY, NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -35,7 +35,7 @@
  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
  * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  */
 
 /** @file
@@ -96,10 +96,10 @@
 #define CENTRAL_LINK_COUNT              0                                           /**< Number of central links used by the application. When changing this number remember to adjust the RAM settings*/
 #define PERIPHERAL_LINK_COUNT           1                                           /**< Number of peripheral links used by the application. When changing this number remember to adjust the RAM settings*/
 
-#define DEVICE_NAME                     "Nordic_Template"                           /**< Name of device. Will be included in the advertising data. */
+#define DEVICE_NAME                     "Gerasimchuk_Template"                           /**< Name of device. Will be included in the advertising data. */
 #define MANUFACTURER_NAME               "NordicSemiconductor"                       /**< Manufacturer. Will be passed to Device Information Service. */
 #define APP_ADV_INTERVAL                300                                         /**< The advertising interval (in units of 0.625 ms. This value corresponds to 187.5 ms). */
-#define APP_ADV_TIMEOUT_IN_SECONDS      180                                         /**< The advertising timeout in units of seconds. */
+#define APP_ADV_TIMEOUT_IN_SECONDS      20                                         /**< The advertising timeout in units of seconds. */
 
 #define APP_TIMER_PRESCALER             0                                           /**< Value of the RTC1 PRESCALER register. */
 #define APP_TIMER_OP_QUEUE_SIZE         4                                           /**< Size of timer operation queues. */
